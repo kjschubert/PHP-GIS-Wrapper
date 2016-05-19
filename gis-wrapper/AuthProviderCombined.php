32bad103530ace1e23116600d8d221a7f495e262
Karl Johann Schubert <karljohann@familieschubi.de>
2016-05-19 23:55:41 +0200

    AuthProviderCombined retries failed request up to three times

--- gis-wrapper/AuthProviderCombined.php
+++ gis-wrapper/AuthProviderCombined.php
@@ -138,53 +138,64 @@ class AuthProviderCombined implements AuthProvider {
         curl_setopt($req, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($req, CURLOPT_COOKIEFILE, "");
         curl_setopt($req, CURLOPT_SSL_VERIFYPEER, $this->_verifyPeer);
-        $res = curl_exec($req);
 
         // get token and expiration date from cookies;
+        $attempts = 0;
         $token = $expire = false;
-        preg_match_all('/^Set-Cookie:\s*([^\n]*)/mi', $res, $cookies);
-        foreach($cookies[1] as $c) {
-            $c = explode('; ', $c);
-            foreach($c as $cookie) {
-                parse_str($cookie, $cookie);
-                if(isset($cookie["expa_token"])) $token = trim($cookie["expa_token"]);
-                if(isset($cookie["Expires"])) $expire = @strtotime($cookie["Expires"]);
+        while(!$token && $attempts < 3) {
+            $res = curl_exec($req);
+            preg_match_all('/^Set-Cookie:\s*([^\n]*)/mi', $res, $cookies);
+            foreach($cookies[1] as $c) {
+                $c = explode('; ', $c);
+                foreach($c as $cookie) {
+                    parse_str($cookie, $cookie);
+                    if(isset($cookie["expa_token"])) $token = trim($cookie["expa_token"]);
+                    if(isset($cookie["Expires"])) $expire = @strtotime($cookie["Expires"]);
+                }
             }
+            $attempts++;
         }
 
-        if($token !== false && $token !== null) {
+        if($token != false) {
             // try to get current person to check if token is valid
             $req2 = curl_init('https://gis-api.aiesec.org/v2/current_person.json?access_token=' . $token);
             curl_setopt($req2, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($req2, CURLOPT_SSL_VERIFYPEER, $this->_verifyPeer);
-            try {
+            $attempts = 0;
+            $current_person = false;
+            while(!$current_person && $attempts < 3) {
                 $current_person = json_decode(curl_exec($req2));
-            } catch (Exception $e) {
-                throw new InvalidAPIResponseException("Could not load current person.");
+                if($current_person == null) {
+                    $current_person = false;
+                    $attempts++;
+                }
             }
 
-            if(isset($current_person->status)) {
+            if($current_person === false) {
+                throw new InvalidAPIResponseException("Could not load current person.");
+            } else if(isset($current_person->status)) {
                 if($current_person->status->code == 403 && $current_person->status->message == "Active role required to view this content.") {
                     $this->_type = false;
 
                     curl_setopt($req, CURLOPT_URL, 'https://auth.aiesec.org/oauth/authorize?redirect_uri=https%3A%2F%2Fopportunities.aiesec.org%2Fauth&response_type=code&client_id=43e49fdc8581a196cafa9122c2e102aa22df72a0e8aa6d5a73f27afa56fe8890');
-                    $res = curl_exec($req);
+                    $attempts = 0;
+                    $content = null;
+                    while($content == null && $attempts < 3) {
+                        $res = curl_exec($req);
+                        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $res, $cookies);
+                        foreach($cookies[1] as $c) {
+                            parse_str($c, $cookie);
+                            if(isset($cookie["aiesec_token"])) {
+                                $content = json_decode($cookie["aiesec_token"]);
+                            }
+                        }
+                        $attempts++;
+                    }
                     curl_close($req);
 
-                    // get token cookie;
-                    $token = $expire = false;
-                    preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $res, $cookies);
-                    foreach($cookies[1] as $c) {
-                        parse_str($c, $cookie);
-                        if(isset($cookie["aiesec_token"])) {
-                            $content = json_decode($cookie["aiesec_token"]);
-                            $token = $content->token->access_token;
-                            $expire = @strtotime($content->token->expires_at);
-                        }
-                    }
-
-                    if($token !== false && $token !== null) {
-                        $this->_token = $token;
+                    if($content !== null) {
+                        $this->_token = $content->token->access_token;
+                        $expire = @strtotime($content->token->expires_at);
                         if($expire !== false && $expire > 0) {
                             $this->_expires_at = $expire;
                         } else {
@@ -192,18 +203,20 @@ class AuthProviderCombined implements AuthProvider {
                             $this->_expires_at = time() + 3600;
                         }
 
-                        curl_setopt($req2, CURLOPT_URL, 'https://gis-api.aiesec.org/v2/current_person.json?access_token=' . $token);
-                        try {
+                        curl_setopt($req2, CURLOPT_URL, 'https://gis-api.aiesec.org/v2/current_person.json?access_token=' . $this->_token);
+                        $attempts = 0;
+                        $this->_currentPerson = false;
+                        while(!$this->_currentPerson && $attempts < 3) {
                             $this->_currentPerson = json_decode(curl_exec($req2));
-                        } catch (Exception $e) {
-                            curl_close($req2);
-                            throw new InvalidAPIResponseException("Could not load current person.");
+                            if($this->_currentPerson == null) {
+                                $this->_currentPerson = false;
+                                $attempts++;
+                            }
                         }
-                        curl_close($req2);
 
                         if(!isset($this->_currentPerson->person)) {
                             if(isset($this->_currentPerson->status)) {
-                                throw new InvalidAPIResponseException($res->status->message);
+                                throw new InvalidAPIResponseException($this->_currentPerson->status->message);
                             } else {
                                 throw new InvalidAPIResponseException("current person endpoint does not contain a person object");
                             }
