5ce8328d1887c2df16745584df07045256eacf4c
Karl Johann Schubert <karljohann@familieschubi.de>
2017-02-24 23:40:17 +0100

    refactored AuthProvider Fix by @ocjojo

--- src/AuthProviderCombined.php
+++ src/AuthProviderCombined.php
@@ -38,7 +38,7 @@ class AuthProviderCombined implements AuthProvider {
     /**
      * @var bool
      */
-    private $_type;
+    protected $_type;
 
     /**
      * @var bool
@@ -157,62 +157,215 @@ class AuthProviderCombined implements AuthProvider {
      *
      * function that performs a login with GIS auth to get a new access token
      *
-     * @throws InvalidAPIResponseException if current person endpoint respond invalid data
-     * @throws InvalidAuthResponseException if the response does not contain the access token
      * @throws InvalidCredentialsException if the username or password is invalid
      */
-    private function generateNewToken() {
-        // assume type is EXPA
+    protected function generateNewToken() {
+        // first assume that the type is EXPA
         $this->_type = true;
-        $auth = new AuthProviderEXPA($this->_username, $this->_password, $this->_verifyPeer);
-        $this->_token = $auth->getToken();
-        $this->_expires_at = $auth->getExpiresAt();
-        try {
-            $current_person = $this->getCurrentPerson();
-        } catch (InvalidAPIResponseException $e){
-            // if no person object, assume it is an OP account
+
+        // run the GIS Auth Flow for EXPA
+        $this->GISauthFlow('https://auth.aiesec.org/oauth/authorize?redirect_uri=https%3A%2F%2Fexperience.aiesec.org%2Fsign_in&response_type=code&client_id=349321fd15814e9fdd2c5abe062a6fb10a27a95dd226fce287adb6c51d3de3df');
+
+        // if the token is not valid
+        if(!$this->getCurrentPerson()) {
+            // assume type is OP
             $this->_type = false;
-            $auth = new AuthProviderOP($this->_username, $this->_password, $this->_verifyPeer);
-            $this->_token = $auth->getToken();
-            $this->_expires_at = $auth->getExpiresAt();
-            try {
-                $this->requestCurrentPerson();
-            } catch (InvalidAPIResponseException $e){
-                throw new InvalidAPIResponseException("Could not validate Account.");
+
+            // run the GIS Auth Flow for OP
+            $this->GISauthFlow('https://auth.aiesec.org/oauth/authorize?redirect_uri=https%3A%2F%2Fopportunities.aiesec.org%2Fauth&response_type=code&client_id=43e49fdc8581a196cafa9122c2e102aa22df72a0e8aa6d5a73f27afa56fe8890');
+
+            // if token is still not valid throw a exception
+            if(!$this->getCurrentPerson()) {
+                throw new InvalidCredentialsException('Could not get a valid token');
             }
         }
     }
 
+    /**
+     * GISauthFlow
+     *
+     * template to perform a login against one of the GIS interfaces
+     *
+     * @param $url string interface specific login url
+     * @return bool
+     * @throws InvalidAuthResponseException
+     */
+    protected function GISauthFlow($url) {
+        // prepare curl request
+        $req = curl_init();
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($req, CURLOPT_HEADER, 1);
+        curl_setopt($req, CURLOPT_SSL_VERIFYPEER, $this->_verifyPeer);
+        curl_setopt($req, CURLOPT_COOKIEFILE, $this->_session);
+        curl_setopt($req, CURLOPT_COOKIEJAR, $this->_session);
+        curl_setopt($req, CURLOPT_FOLLOWLOCATION, true);
+
+        // first check if we are working with an existing session, because if this fails we either need to run a normal login flow
+        if(file_exists($this->_session)) {
+            $res = true;
+            for($i = 0; $i < 3; $i++) {
+                // set url
+                curl_setopt($req, CURLOPT_URL, $url);
+
+                // run request
+                $res = curl_exec($req);
+
+                // check response
+                if($res !== false) {
+                    try {
+                        // if successful, check for token
+                        $this->proceedToken($res);
+
+                        // if we are still here there was no exception so we got a token
+                        curl_close($req);
+                        return true;
+                    } catch (InvalidAuthResponseException $e) {
+                        // if there was no token, this can mean that the session was invalid, thereby break the loop and skip to normal login procedure
+                        break;
+                    }
+                }
+            }
+        }
+
+        // if we are still here we need to run a normal login, therefore we need a CSRF token first
+        $data = false;
+        for($i = 0; $i < 3; $i++) {
+            curl_setopt($req, CURLOPT_URL, $url);
+            $res = curl_exec($req);
+            if($res !== false) {
+                // try to find csrf token in response
+                preg_match( '/<meta.*content="(.*)".*name="csrf-token"/', $res, $match );
+
+                // check for result
+                if(isset($match[1])) {
+                    // prepare post data if we got a token and skip further loop runs
+                    $data = "user%5Bemail%5D=" . urlencode($this->_username) . "&user%5Bpassword%5D=" . urlencode($this->_password) . '&authenticity_token=' . urlencode($match[1]);
+                    break;
+                }
+            }
+        }
+
+        // throw exception if we did not get a csrf token
+        if($data === false) {
+            curl_close($req);
+            throw new InvalidAuthResponseException("Did not get a CSRF token.");
+        }
+
+        // through we ran the csrf request against the specific url our session is initiated and we can just perform an login
+        for($i = 0; $i < 3; $i++) {
+            // update the curl handle
+            curl_setopt($req, CURLOPT_URL, 'https://auth.aiesec.org/users/sign_in');
+            curl_setopt($req, CURLOPT_POST, true);
+            curl_setopt($req, CURLOPT_POSTFIELDS, $data);
+
+            // run request
+            $res = curl_exec($req);
+
+            // check response
+            if($res !== false) {
+                // check for token
+                $this->proceedToken($res);
+
+                // if we are still here we succeeded
+                curl_close($req);
+                return true;
+            }
+        }
+
+        // close curl handle
+        curl_close($req);
+
+        // if we are still here we have to throw an exception
+        throw new InvalidAuthResponseException("Could not login.");
+    }
+
+    /**
+     * proceedToken
+     *
+     * proceeds an gis auth response and extracts the token
+     *
+     * @param $res string GIS auth response
+     * @throws InvalidAuthResponseException
+     * @throws InvalidCredentialsException
+     */
+    private function proceedToken($res) {
+        // get token and expiration date from cookie
+        $token = $expire = false;
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $res, $cookies);
+        foreach($cookies[1] as $c) {
+            parse_str($c, $cookie);
+            if(isset($cookie["aiesec_token"])) {
+                $content = json_decode($cookie["aiesec_token"]);
+                $token = $content->token->access_token;
+                $expire = @strtotime($content->token->expires_at);
+            } elseif(isset($cookie["expa_token"])) {
+                $token = $cookie["expa_token"];
+            } elseif(isset($cookie['Expires'])); {
+                $expires = @strtotime($cookie['Expires']);
+            }
+        }
+
+        // check if we got a token
+        if($token !== false && $token !== null) {
+            $this->_token = $token;
+            if($expire !== false && $expire > 0) {
+                $this->_expires_at = $expire;
+            } else {
+                // if we can not parse the expiration date, assume 1h
+                $this->_expires_at = time() + 3600;
+            }
+        } else  {
+            if(strpos($res, "<h2>Invalid email or password.</h2>") !== false) {
+                throw new InvalidCredentialsException("Invalid email or password");
+            } else {
+                throw new InvalidAuthResponseException("The GIS auth response does not match the requirements.");
+            }
+        }
+    }
+
+    /**
+     * requestCurrentPerson
+     *
+     * check if token is valid by requesting the current person object
+     *
+     * @return bool
+     * @throws InvalidAPIResponseException
+     */
     private function requestCurrentPerson(){
         // prepare curl request
         $req = curl_init();
         curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($req, CURLOPT_SSL_VERIFYPEER, $this->_verifyPeer);
+
         // try to get current person to check if token is valid
-        $attempts = 0;
-        $current_person = false;
-        while(!$current_person && $attempts < 3) {
+        for($i = 0; $i < 3; $i++) {
             // run request
             curl_setopt($req, CURLOPT_URL, 'https://gis-api.aiesec.org/v2/current_person.json?access_token=' . $this->getToken());
-            $current_person = json_decode(curl_exec($req));
+            $res = curl_exec($req);
 
             // check response
-            if($current_person == null) {
-                $current_person = false;
+            if($res !== false) {
+                // decode response
+                $currentPerson = json_decode($res);
+
+                // check content
+                if(isset($currentPerson->person)) {
+                    curl_close($req);
+                    $this->_currentPerson = $currentPerson;
+                    return true;
+                } elseif($currentPerson !== null) {
+                    // if we got a valid json object back, but without a person the token is invalid
+                    curl_close($req);
+                    return false;
+                }
             }
-            $attempts++;
         }
-        if($current_person !== false && isset($current_person->person)) {
-            curl_close($req);
-            $this->_currentPerson = $current_person;
-        } else {
-            curl_close($req);
-            if(isset($currentPerson->status)) {
-                throw new InvalidAPIResponseException($currentPerson->status->message);
-            } else {
-                throw new InvalidAPIResponseException("Could not load current person after $attempts attempts.");
-            }
-        }
+
+        // close curl handle
+        curl_close($req);
+
+        // throw exception if we did not get a result
+        throw new InvalidAPIResponseException("Could not load current person after 3 attempts.");
     }
 
 }
